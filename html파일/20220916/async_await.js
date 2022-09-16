c7b7d53153dc072f34dabdfbddbde2d8beff7f92
seungbeen <seungbin803@gmail.com>
2022-09-16 14:12:32 +0900

    비동기 작업 async & await

---
+++ html파일/20220916/async_await.js
@@ -0,0 +1,15 @@
+async function f() {
+    let promise = new Promise((resolve, reject) => {
+        setTimeout(() => resolve("완료!"), 1000)
+    });
+    // 프라미스가 이행될 때까지 잠시 중단되어 1초를 기다리게 됨
+    // (즉, 실제로는 비동기 작업을 진행해도, 바깥에서는 순차적인 작업을 진행하는 것처럼 코드를 작성 가능)
+    let result = await promise;
+    alert(result); // "완료!"
+    }
+    // fire-and-forget
+    f();
+    console.log("Hello");
+
+    // 실행이 동시에 되는 듯한 느낌이다
+    
